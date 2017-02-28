fdaaefd4f253492e7201ff5523e6979745dd5ff8
Pavitra <paviambi@gmail.com>
2017-02-28 10:44:24 +0530

    JPA error fixed

---
+++ src/com/pavitra/business/CardType.java
@@ -0,0 +1,34 @@
+package com.pavitra.business;
+/**
+ * Simple Enum which represents accepted credit card types.
+ *
+ * @author Rishikesh Jha
+ */
+public enum CardType {
+
+   VISA("visa"), MASTER_CARD("masterCard");
+   
+   private final String name;
+   
+   private CardType(String name) {
+      this.name = name;
+   }
+   
+   /**
+    * Static method which creates and returns an Enum based on the provided String
+    * value.
+    *
+    * @param stringCardType A String representation of the required Enum
+    * @return Either the required Enum or a null for an invalid input
+    */
+   public static CardType fromString(String stringCardType) {
+      CardType cardType = null;
+      for (CardType ct : CardType.values()) {
+         if (stringCardType.equals(ct.name)) {
+            cardType = ct;
+            break;
+         }
+      }
+      return cardType;
+   }
+}
